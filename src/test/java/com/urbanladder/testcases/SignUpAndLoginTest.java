1938e33c8cd62136665d51d6b3df5f3b3cbfe13c
Shriraj <shriraj.ghorpade29@gmail.com>
2023-04-02 12:23:22 +0530

    Sign up functionality test case.

---
+++ src/test/java/com/urbanladder/testcases/SignUpAndLoginTest.java
@@ -0,0 +1,18 @@
+package com.urbanladder.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.urbanladder.base.TestBase;
+
+public class SignUpAndLoginTest extends TestBase{
+
+	@Test(priority = 1, enabled = false, description = "Validation of Sign Up Functionality")
+	public void signUpTest() {
+		homePg.clickOnSignUp();
+		signUpPg.verifySignUpPage();
+		signUpPg.enterDetailsAndClickSignUp("anonymous@gmail.com", "TestPass@123");
+		boolean isSuccessful = homePg.verifySignUpOrLoginIsSuccessful();
+		Assert.assertTrue(isSuccessful);
+	}
+}
